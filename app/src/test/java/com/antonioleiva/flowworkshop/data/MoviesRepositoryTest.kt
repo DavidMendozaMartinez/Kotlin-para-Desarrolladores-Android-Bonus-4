852ebe3d07a59c6277b4b8b89ca9800b988c162e
David Mendoza <david.mendoza.mnez@gmail.com>
2021-04-01 17:47:59 +0200

    Create MainViewModelTest and MoviesRepositoryTest

---
+++ app/src/test/java/com/antonioleiva/flowworkshop/data/MoviesRepositoryTest.kt
@@ -0,0 +1,28 @@
+package com.antonioleiva.flowworkshop.data
+
+import com.antonioleiva.flowworkshop.CoroutinesTestRule
+import com.antonioleiva.flowworkshop.data.domain.MoviesRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class MoviesRepositoryTest {
+
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
+
+    @Test(expected = TimeoutCancellationException::class)
+    fun `After timeout, an exception is thrown`() = runBlockingTest {
+        val repository = MoviesRepository(
+            FakeLocalDataSource(),
+            FakeRemoteDataSource(delay = 6_000)
+        )
+
+        repository.checkRequireNewPage(0)
+
+        advanceTimeBy(5_000)
+    }
+}
---
+++ app/src/test/java/com/antonioleiva/flowworkshop/ui/MainViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.antonioleiva.flowworkshop.ui
+
+import com.antonioleiva.flowworkshop.CoroutinesTestRule
+import com.antonioleiva.flowworkshop.data.FakeLocalDataSource
+import com.antonioleiva.flowworkshop.data.FakeRemoteDataSource
+import com.antonioleiva.flowworkshop.data.domain.MoviesRepository
+import com.antonioleiva.flowworkshop.data.fakeMovies
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class MainViewModelTest {
+
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
+
+    @Test
+    fun `Listening to movies Flow emits the list of movies from the server`() = runBlockingTest {
+        val repository = MoviesRepository(FakeLocalDataSource(), FakeRemoteDataSource(fakeMovies))
+        val vm = MainViewModel(repository)
+
+        vm.movies.collect {
+            Assert.assertEquals(fakeMovies, it)
+        }
+    }
+}
